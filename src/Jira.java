b46b73334cda1148faae5407beef0a7f3dcd6286
JiaxinSu <jiaxin.su@idt.net>
2016-07-05 16:28:45 -0400

    JIRA fixed

--- src/Jira.java
+++ src/Jira.java
@@ -12,8 +12,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.thoughtworks.selenium.webdriven.JavascriptLibrary;
 
-public class Jira extends Thread{
+
+public class Jira{
 	
 	public static void main (String[] args){
 		Jira testJira = new Jira("", "cliang", "Shortbanana24"); 
@@ -116,8 +118,10 @@ public class Jira extends Thread{
 			}
 		}
 
+		System.out.println("before span dropdown");
 		driver.findElement(By.cssSelector("span.dropdown-text")).click();
 		driver.findElement(By.id("attach-file")).click();
+		System.out.println("after the attach-file dropdown is clicked");
 		
 		for (int second = 0;; second++) {
 			if (second >= 120) break;
@@ -146,82 +150,19 @@ public class Jira extends Thread{
 
 				System.out.println("switched to active element");
 				//driver.findElement(By.cssSelector("label.issue-drop-zone__file.upfile")).sendKeys(pics[y]);
-				//driver.findElement(By.xpath("//label[contains(@class, 'issue-drop-zone__button')]")).click();
-				/* most plausible*/ 
-				//driver.findElement(By.xpath("//label[contains(@class, 'issue-drop-zone__button')]")).sendKeys(pics[y]);
-				/*WebElement fileInput = driver.findElement(By.name("issue-drop-zone__button")); 
-				System.out.println("found or not: " + driver.findElement(By.name("issue-drop-zone__button")));
-				fileInput.sendKeys(pics[y]); 
-				System.out.println(pics[y]);
-				*/				
-				//driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone__button')]/button[2]")).click();
-				//System.out.println("pic count: " + picCount + pics[y]); 
-				//driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button")).sendKeys(pics[y]);
-			/*
-		
-				if (driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button"))== null){
-					System.out.println("null 1");
-				}
-				else{
+				//driver.findElement(By.xpath("//label[contains(@class, 'issue-drop-zone__button aui-button')]")).sendKeys(pics[y]);
 				
-					System.out.println("1");
-					
-					File Ifile = new File(pics[y]); 
-					WebElement inputField = driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button"));
-					inputField.sendKeys(Ifile.getAbsolutePath());
-			
-					//System.out.println(driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button")));
-					//driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button")).click(); 
-					//driver.findElement(By.id("attach-file-submit")).click(); 
-			
-					String jsScript = "var input = document.getElementsByTagName('input')[0];"
-					        +"input.value='/BOSS_Screenshots_v4.81';";
-					JavascriptExecutor executor = (JavascriptExecutor)driver;
-				}
-		*/
-				//driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]")).sendKeys(pics[y]);
-				if (driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]"))== null ){
-					System.out.println("null 2");
-				}
-				else{
-					System.out.println("2");
-					
-					String jsScript = "var input = document.getElementsByTagName('label')[0];"
-					        +"input.value='/C/BOSS_Screenshots_v4.81';" + "console.log(input)";
-					System.out.println("jsScript script: " + jsScript);
-					
-					JavascriptExecutor executor = (JavascriptExecutor)driver;
-					executor.executeScript(jsScript);
-					/*
-					File fileI = new File(pics[y]);
-					WebElement fieldInput = driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]")); 
-					fieldInput.sendKeys(fileI.getAbsolutePath());
-					*/
-					//System.out.println(driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]"))); 
-					//driver.findElement(By.cssSelector("label.issue-drop-zone__button.aui-button")).click(); 
-					//driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]")).sendKeys(pics[y]);
-					//driver.findElement(By.id("attach-file-submit")).click(); 
-				
-				}
-
 				System.out.println("pic count: " + picCount);
 				System.out.println(pics[y]); 
+				//WebElement input = driver.findElement(By.xpath("//label[contains(@class, 'issue-drop-zone__button aui-button')]"));
+				WebElement input = driver.findElement(By.className("upfile"));
+				//input.issue-drop-zone__file.ignore-inline-attach.upfile
+				//((JavascriptExecutor) driver).executeScript("arguments[0].click();", input);
+				JavascriptLibrary jsLib = new JavascriptLibrary(); 
+				jsLib.executeScript(driver, "document.getElementsByClassName('upfile')[0].style.display = 'block';");
+				input.sendKeys(pics[y]);
+				//driver.findElement(By.xpath("//label[contains(@class, 'issue-drop-zone__button aui-button')]")).click();
 				
-				// call Click in a separate thread, and then call the AutoIt script. Once the AutoIt script closes the dialog box, 
-				// the click will return, and the rest of your test can continue.
-			/*	try {
-					run(); // call child thread
-				}
-				catch (Exception e) {
-					System.out.println("exception while running the child thread");
-					e.printStackTrace();
-				} */
-				// Main Thread clicks on the button
-				driver.findElement(By.xpath("//div[contains(@class, 'issue-drop-zone -dui-type-parsed')]/label[contains(@class, 'issue-drop-zone__button aui-button')]")).click();
-				
-				
-				
-
 			}
 			catch(Exception e){
 				e.printStackTrace();
